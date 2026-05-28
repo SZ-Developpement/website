e6019b3c9f3b5d40467fc10e954d79e90bf41f86
dealexis <flytzitv@gmail.com>
2026-05-28 23:35:19 +0200

    feat: Added main content to the administration page with a dashboard title

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -6,7 +6,9 @@ export default function AdminPage() {
   return (
     <>
       <HeaderAdmin />
-      <main></main>
+      <main className="flex items-center justify-center h-screen">
+        <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+      </main>
     </>
   );
 }
